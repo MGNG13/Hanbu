d0c86c708c990115bc04398c5c946c0003e178b6
MGNG13 <olafmagnusolafnorgaard@gmail.com>
2022-09-18 23:59:26 -0500

    Version 2. Commited on September 18 2022, 23:59:26.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#git remote remove origin
+git remote remove origin
 git remote add origin https://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@github.com/MGNG13/Hanbu
 git add -A
 git commit -am "Version $(git rev-list --all --count). Commited on $(date '+%B %e %Y, %T')."
